54a7a8c273b415ebb878c95b190db155296d05a6
Mir Sameed Ali <mirsameedali@Mirs-MacBook-Pro.local>
2020-03-08 15:13:50 +0530

    map added

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
+const App = () => {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 3,
+    overflow: "hidden"
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapStyle="mapbox://styles/mirsamali/ck7ioeyee0ch11io7kn7oebju"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={setViewport}
+    />
+  );
+};
+
+export default App;
